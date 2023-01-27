0d9e9a8e9ecc83c549fad3c104cf061b2d98154d
Daniel Leinberger <d.leinberger@xdev-software.de>
2023-01-27 11:32:19 +0100

    Updated license doc
    Warning! Compile Error due to maven-compiler-plugin.
    Need to be fixed, before usage.

--- src/test/java/xdev/db/ConnectionConfigurationTest.java
+++ src/test/java/xdev/db/ConnectionConfigurationTest.java
@@ -1,43 +1,21 @@
 /*
- * Copyright (C) 2007-2013 by XDEV Software, All Rights Reserved.
+ * XDEV Application Framework - XDEV Application Framework
+ * Copyright © 2003 XDEV Software (https://xdev.software)
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License version 3.0 as published by the Free Software Foundation.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- */
-
-package xdev.db;
-
-/*-
- * #%L
- * XDEV Application Framework
- * %%
- * Copyright (C) 2003 - 2020 XDEV Software
- * %%
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- * 
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Lesser Public License for more details.
- * 
- * You should have received a copy of the GNU General Lesser Public
- * License along with this program.  If not, see
- * <http://www.gnu.org/licenses/lgpl-3.0.html>.
- * #L%
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+package xdev.db;
 
 import static org.junit.Assert.fail;
 
